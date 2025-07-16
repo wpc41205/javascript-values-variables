37ed6daa0486b5426052e9557389150ca8e4ced2
Patchara <sombrousnorn@gmail.com>
2025-07-16 16:03:06 +0700

    retest(ex-4)

--- ex-4.js
+++ ex-4.js
@@ -3,11 +3,11 @@
 
 //Exercise #4: What is Expression Start coding here
 130 + 300; // Expression
-let zero = 1000 - 1000; // Expression
+let zero = 1000 - 1000;
 20 * 120; // Expression
 1000 / 2; // Expression
-typeof true; // Expression
-let name = "John"; // Expression
+typeof true; 
+let name = "John"; 
 3 > 5; // Expression
 10 == 100; // Expression
 200; // Expression
